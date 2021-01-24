1efd27c3c5a07c1079d281e239a683a589b626c8
mkoterski <matthiaskoterski@gmail.com>
2021-01-24 12:36:19 +0100

    updates for pull request testing
    updates for pull request testing

---
+++ display-password.py
@@ -0,0 +1,1 @@
+display-password.py
---
+++ handshakes-zip-dl.py
@@ -0,0 +1,85 @@
+import logging
+import json
+import os
+import glob
+import zipfile
+from io import BytesIO
+
+import pwnagotchi
+import pwnagotchi.plugins as plugins
+
+from flask import abort
+from flask import send_from_directory, send_file
+from flask import render_template_string
+
+TEMPLATE = """
+{% extends "base.html" %}
+{% set active_page = "handshakes" %}
+
+{% block title %}
+    {{ title }}
+{% endblock %}
+
+{% block content %}
+    <a class="ui-btn" href="/plugins/handshakes-zip-dl/all">Download Zip</a>
+    <ul id="list" data-role="listview" style="list-style-type:disc;">
+        {% for handshake in handshakes %}
+            <li class="file">
+                <a href="/plugins/handshakes-zip-dl/{{handshake}}">{{handshake}}</a>
+            </li>
+        {% endfor %}
+    </ul>
+{% endblock %}
+"""
+
+class HandshakesDL(plugins.Plugin):
+    __author__ = 'me@sayakb.com'
+    __version__ = '0.1.0'
+    __license__ = 'GPL3'
+    __description__ = 'Download all handshake captures from web-ui as single zip file.'
+
+    def __init__(self):
+        self.ready = False
+
+    def on_loaded(self):
+        logging.info("[HandshakesZipDL] plugin loaded")
+
+    def on_ready(self, agent):
+        self.config = agent.config()
+        self.ready = True
+
+    def on_internet_available(self, agent):
+        self.config = agent.config()
+        self.ready = True
+
+    def on_webhook(self, path, request):
+        if not self.ready:
+            return "Plugin not ready"
+
+        if path == "/" or not path:
+            handshakes = glob.glob(os.path.join(self.config['bettercap']['handshakes'], "*.pcap"))
+            handshakes = [os.path.basename(path)[:-5] for path in handshakes]
+            return render_template_string(TEMPLATE,
+                                    title="Handshakes | " + pwnagotchi.name(),
+                                    handshakes=handshakes)    
+        elif path == "all":
+            logging.info(f"[HandshakesDL] creating Zip-File in memory")
+            memory_file = BytesIO()
+            with zipfile.ZipFile(memory_file, 'w') as zf:
+                files = glob.glob(os.path.join(self.config['bettercap']['handshakes'], "*.pcap"))
+                try:
+                    for individualFile in files:
+                        zf.write(individualFile)
+                except Exception as e:
+                    logging.error(f"[HandshakesDL] {e}")
+                    abort(404)
+            memory_file.seek(0)
+            logging.info(f"[HandshakesDL] serving handshakes.zip")
+            return send_file(memory_file, attachment_filename='handshakes.zip', as_attachment=True)
+        else:
+            dir = self.config['bettercap']['handshakes']
+            try:
+                logging.info(f"[HandshakesDL] serving {dir}/{path}.pcap")
+                return send_from_directory(directory=dir, filename=path+'.pcap', as_attachment=True)
+            except FileNotFoundError:
+                abort(404)
