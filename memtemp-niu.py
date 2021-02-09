3d3b309273d5fff6bc9fa18946493dc4d8536ba6
mkoterski <matthiaskoterski@gmail.com>
2021-02-09 21:09:51 +0100

    Update memtemp-niu.py

--- memtemp-niu.py
+++ memtemp-niu.py
@@ -32,7 +32,7 @@ import logging
 
 class MemTemp(plugins.Plugin):
     __author__ = 'https://github.com/xenDE'
-    __version__ = '1.0.3.3.3'
+    __version__ = '1.0.3.3.4'
     __license__ = 'GPL3'
     __description__ = 'A plugin that will display memory/cpu usage and temperature'
 
